0b2d82f8c1a549b56942bd9302489497326b92cb
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-03-16 11:45:30 -0300

    começando estilização da landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
 
-function Landing(){
-  return(
-    <h1>Landing</h1>
-  )
+import './style.css'
+
+import LogoImg from "../../assets/images/logo.svg";
+import LandingImg from "../../assets/images/landing.svg";
+
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+function Landing() {
+  return (
+    <div className="page-landing">
+      <div className="page-landing-content">
+        <div className="logo-container">
+          <img src={LogoImg} alt="Logo proffy" />
+          <h2>Sua platarforma de estudo online.</h2>
+        </div>
+
+        <img src={LandingImg} alt="landing" className="hero-image" />
+
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="Estudar" />
+            Estudar
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
